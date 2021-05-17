2b80c5b4fd3da45598af5f9872069dc9e282f97d
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-05-17 16:15:03 +0200

    Add UI Folder

---
+++ react-next-steps/src/components/UI/Card.js
@@ -0,0 +1,12 @@
+import './Card.css';
+
+function Card(props) {
+  const classes = "card" + props.className;
+  
+  return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
+
+//children will be whatever stays between opening and closing tags of this component!!
+//using children props you can substitute the generic div wrapper tag with a customised one, in this case Card, which will wrap all the other components (children)
