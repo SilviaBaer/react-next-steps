3c64e46a6e6a74fcf24a77dfedd5b80b5881c0d1
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-05-17 16:16:16 +0200

    clean up App.js file

--- react-next-steps/src/App.js
+++ react-next-steps/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Expenses from "./components/Expenses/Expenses"
+import NewExpense from "./components/NewExpense/NewExpense"
+
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense/>
+      <Expenses items={expenses}/>
     </div>
   );
 }
 
 export default App;
+
+//app component is the stamm component renderd into single HTML page with help of react dom instructions
+//components is = HTML elements
+
+//LESSON 36. PASSING DATA VIA PROPS 
+//it is possible to pass data and reuse components with the PROPS (attributes in  HTML) concept
+//1/2 mapping components (attributes list)
+//2/2 parameter = props (object that holds all the received values we get as attributes as properties = props in ExpenseItem.js)
+//key value pair = (key = attribute name (title-amount-date)) + (value = value)
+//instead of hardcoding each item I can reuse the component passing data via props 
+//... that means that once the props are stated than it will loop in the array for each component and render it. 
