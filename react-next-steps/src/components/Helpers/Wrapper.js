c751591c74ded2a2678e2a37082467a9a0be144c
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-05-17 19:14:59 +0200

    ✨ add Wrapper component as Helper

---
+++ react-next-steps/src/components/Helpers/Wrapper.js
@@ -0,0 +1,8 @@
+const Wrapper = props => {
+    return props.children;
+};
+
+export default Wrapper;
+
+//children prop hold all the contents you are passing beween the <> of your custom component
+//ready to be exported  as <Wrapper><Wrapper/> and substitute the div component (div soup)
