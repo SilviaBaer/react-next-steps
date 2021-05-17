d389be1d8786b9e090033a4901f210981842b31e
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-05-17 17:48:01 +0200

    🔥 remove redundant comments

--- react-next-steps/src/components/Expenses/Expenses.js
+++ react-next-steps/src/components/Expenses/Expenses.js
@@ -44,29 +44,3 @@ export default Expenses;
     //const expenseDate = new Date (2021, 2, 28);
     //const expenseTitle = "Car Insurance";
     //const expenseAmount = 25.98;
-{/*OLD VERSION 
-  17/5    
-  now this items are mapped => creating an array from a another array passing the function 
-      with the result that now we have a dynamic list that can also be updated dinamically (new item ...)
-(continues in app.js, because the array that needs to be updated is hosted there)
-      <ExpenseItem 
-      title={props.items[0].title} 
-      amount={props.items[0].amount} 
-      date={props.items[0].date}>
-      </ExpenseItem>
-      <ExpenseItem 
-      title={props.items[1].title} 
-      amount={props.items[1].amount} 
-      date={props.items[1].date}>
-      </ExpenseItem>
-      <ExpenseItem
-      title={props.items[2].title} 
-      amount={props.items[2].amount} 
-      date={props.items[2].date}>
-      </ExpenseItem>
-      <ExpenseItem
-      title={props.items[3].title} 
-      amount={props.items[3].amount} 
-      date={props.items[3].date}>
-      </ExpenseItem> */}
-    //comments
